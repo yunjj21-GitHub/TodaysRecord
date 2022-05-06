df05664cc112287a43ee13b58b15d7be9a5dbb8e
yunjung <yunjj21@hs.ac.kr>
2022-05-06 21:00:35 +0900

    PhotoBooth 클래스 image 프로퍼티 제거
    PhotoBooth 클래스 image 프로퍼티 제거

--- app/src/main/java/com/yunjung/todaysrecord/models/PhotoBooth.kt
+++ app/src/main/java/com/yunjung/todaysrecord/models/PhotoBooth.kt
@@ -5,6 +5,5 @@ data class PhotoBooth (
     var brand : String? = null,
     var name : String? = null, // '인생네컷 - 홍대점'과 같은 풀네임을 저장
     var address : String? = null,
-    var image : String? = null,
     var location : List<Double>? = null // 지도에 표시하기 위한 좌표
 )
